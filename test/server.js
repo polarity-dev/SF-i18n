06c41344e6084b8269ee5d47a47ffbfa0000f7d3
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2016-04-19 18:10:06 +0200

    mods

--- test/server.js
+++ test/server.js
@@ -3,15 +3,24 @@
 const express = require('express');
 const path    = require('path');
 const app     = express();
-const i18n    = require('./../lib/sf-i18n')(app, path.join(__dirname, './locales'));
+const i18n    = require('./../lib/sf-i18n');
 
 app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(i18n);
-console.log(app.locals);
+app.use(i18n.init(app, path.join(__dirname, './locales')));
 
-app.get('*', function(req, res){
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.get('/en', function(req, res){
+  i18n.currentLang = 'EN';
+  res.render('index');
+});
+
+app.get('/it', function(req, res){
+  i18n.currentLang = 'IT';
   res.render('index');
 });
 
