0fec193c03757ee07c7ed1b7587134ddf2c4ff02
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2016-04-20 17:52:40 +0200

    first release

--- test/server.js
+++ test/server.js
@@ -8,19 +8,23 @@ const i18n    = require('./../lib/sf-i18n');
 app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(i18n.init(app, path.join(__dirname, './locales')));
+const options = {
+  cookieName    : 'mywebsite-i18n',
+  defaultLocale : 'IT'
+};
+app.use(i18n.init(app, path.join(__dirname, './locales'), options ));
 
 app.get('/', function(req, res){
   res.render('index');
 });
 
 app.get('/en', function(req, res){
-  i18n.currentLang = 'EN';
+  i18n.currentLocale = 'EN';
   res.render('index');
 });
 
 app.get('/it', function(req, res){
-  i18n.currentLang = 'IT';
+  i18n.currentLocale = 'IT';
   res.render('index');
 });
 
